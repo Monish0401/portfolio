db8eb23e048413aaae2ec5607da1edc5914ac343
Monish C <86337147+Monish0401@users.noreply.github.com>
2025-09-23 12:22:47 +0530

    update hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'motion/react';
-import { ImageWithFallback } from './figma/ImageWithFallback';
+import { ImageWithFallback } from './imgfallback/ImageWithFallback';
 import { unsplash_tool } from '../tools/unsplash';
 
 export function Hero() {
