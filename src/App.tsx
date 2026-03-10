e68dfb6345230b33b6227130497aa427b2c9700e
Monish C <86337147+Monish0401@users.noreply.github.com>
2026-03-10 23:06:51 +0530

    Added Projects section

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Hero } from './components/Hero';
 import { About } from './components/About';
 import { Skills } from './components/Skills';
 import { Experience } from './components/Experience';
-// import { Projects } from './components/Projects';
+import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
 
 export default function App() {
@@ -28,9 +28,9 @@ export default function App() {
           <Experience />
         </section>
         
-{/*         <section id="projects">
+        <section id="projects">
           <Projects />
-        </section> */}
+        </section>
         
         <section id="contact">
           <Contact />
